0932e42aa179c40d7f8e932da31d7650057a312b
Surya <kantalesurya@gmail.com>
2026-02-04 16:24:32 +0530

    Add initial Python practice files for basic syntax, operators, and conditional statements

---
+++ IV-SEM/Python/Practice/M01_Language_&_Programming_Basics/S01/PS01_Python_setup_and_syntax.py
@@ -0,0 +1,5 @@
+user_name = "surya"
+print(user_name)
+name = "a"
+Name = "B"
+print(Name)
---
+++ IV-SEM/Python/Practice/M01_Language_&_Programming_Basics/S02/PS03_operators.py
@@ -0,0 +1,8 @@
+x = 13
+y = 5
+print( x & y )
+print( x | y )
+print( x ^ y )
+print( x << 2 )
+print( x >> 2 )
+
